d0b6d6dcd84b76c1cd13a0721f803340cbbd8074
Ruijia Su <rusu@slurmhead.hpc.itu.dk>
2025-12-09 17:56:12 +0100

    Add .sh file

---
+++ navppo/run_train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=navppo_train
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --error=logs/%x-%j.err
+#SBATCH --partition=scavenge
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --time=04:00:00
+
+source ../venv/bin/activate
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+mkdir -p logs
+
+python run_train_env.py
