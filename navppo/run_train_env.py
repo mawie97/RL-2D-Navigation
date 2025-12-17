6ff8d5fd5008c8a9f06f708baa88f3d44c575b8f
Susu <rusu@itu.dk>
2025-12-17 15:34:14 +0100

    Add log

--- navppo/run_train_env.py
+++ navppo/run_train_env.py
@@ -10,7 +10,7 @@ env_class = MujocoGoalEnv
 
 current_dir = os.path.dirname(__file__)  # scripts/train
 xml_base_dir = os.path.abspath(os.path.join(current_dir, 'layouts', 'train', 'deadend'))
-base_dir = os.path.abspath(os.path.join(current_dir, 'runs', 'Model_1_noise0'))
+base_dir = os.path.abspath(os.path.join(current_dir, 'runs', 'Model_1v2_noise0'))
 xml_paths = sorted([os.path.join(xml_base_dir, f) for f in os.listdir(xml_base_dir) if f.endswith(".xml")])
 
 log_dir = os.path.join(base_dir, "logs")
