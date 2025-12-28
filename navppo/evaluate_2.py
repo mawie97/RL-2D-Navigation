30615ac52cfbc573971491b9b0b14385e08f1421
Susu <rusu@itu.dk>
2025-12-28 15:39:31 +0100

    Add two more evaluate code

--- navppo/evaluate_2.py
+++ navppo/evaluate_2.py
@@ -7,7 +7,7 @@ from torch.utils.tensorboard.writer import SummaryWriter
 from multi_ray_goal_env import MujocoGoalEnv
 
 current_dir = os.path.dirname(__file__)
-xml_base_dir = os.path.abspath(os.path.join(current_dir, 'layouts', 'eval', 'lvl_1_5'))
+xml_base_dir = os.path.abspath(os.path.join(current_dir, 'layouts', 'eval', 'lvl_1_4'))
 xml_paths = sorted([os.path.join(xml_base_dir, f) for f in os.listdir(xml_base_dir) if f.endswith(".xml")])
 
 current_setup = "Model_1_noise0_copy"
